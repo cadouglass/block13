be4c3523acd33d243f88772448bb7a14168ea2b0
Camryn Douglass <camryndouglass@MacBook-Air-6.lan>
2023-07-26 20:09:27 -0400

    add files

---
+++ index.js
@@ -0,0 +1,14 @@
+// "I am a string" is true print true
+
+const string= false
+
+if (!string) {
+    console.log('true');
+}
+
+const num1= 50;
+const num2= 51;
+
+if (num1+num2 ===101) {
+    console.log ('101 is greater than 100');
+}
